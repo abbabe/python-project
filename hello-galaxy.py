6780dbfbb817dd5b1c907abca81b4d222c94e7f2
abbabe <99480208+abbabe@users.noreply.github.com>
2022-12-30 23:28:46 +0300

    Update hello-galaxy.py

--- hello-galaxy.py
+++ hello-galaxy.py
@@ -4,18 +4,3 @@ print (" Hello Galaxy")
 
 # Payment
 
-def computepay(h,r):
-    if h>45:
-        return (40*r) + (h-40)*(r*1.5)
-    else :
-        return (h*r)*g
-
-hrs = input("Enter Hours-Gunluk Çalışma Saati:")
-h=float(hrs)
-rate=input("Enter Rate-Saatlik Ücret:")
-r=float(rate)
-gun=input("Calısılan toplam gun:")
-g=float(gun)
-
-p = computepay(h,r)
-print("Pay-Brut Ucret",p)
